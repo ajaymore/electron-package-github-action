1b72302ae3d11f5f57b2d86ec0bec3b1fa03800c
Ajay <mail@ajaymore.in>
2020-07-28 07:52:37 +0530

    automate version number

--- upload.js
+++ upload.js
@@ -1,5 +1,6 @@
 const { google } = require('googleapis');
 const fs = require('fs');
+const pjson = require('./package.json');
 // const token = require('./token.json');
 // const credentials = require('./credentials.json');
 
@@ -17,12 +18,12 @@ oAuth2Client.setCredentials(token);
 function uploadFile() {
   const drive = google.drive({ version: 'v3', auth: oAuth2Client });
   const fileMetadata = {
-    name: 'MiigoDataLogger-1.0.0_32.exe',
+    name: `MiigoDataLogger-${pjson}_32.exe`,
     parents: ['1_DM_scA9dKI1qThWzUJOPv2LXlV0l1jw'],
   };
   const media = {
     mimeType: 'text/plain',
-    body: fs.createReadStream('dist/MiigoDataLogger-1.0.0_32.exe'),
+    body: fs.createReadStream(`dist/MiigoDataLogger-${pjson}_32.exe`),
   };
   drive.files.create(
     {
@@ -40,12 +41,12 @@ function uploadFile() {
     }
   );
   const fileMetadata2 = {
-    name: 'MiigoDataLogger-1.0.0_64.exe',
+    name: `MiigoDataLogger-${pjson}_64.exe`,
     parents: ['1_DM_scA9dKI1qThWzUJOPv2LXlV0l1jw'],
   };
   const media2 = {
     mimeType: 'text/plain',
-    body: fs.createReadStream('dist/MiigoDataLogger-1.0.0_64.exe'),
+    body: fs.createReadStream(`dist/MiigoDataLogger-${pjson}_64.exe`),
   };
   drive.files.create(
     {
